35c47447833ce7b2121cb72a52a871ffee17c0bb
kiranravindrant <kiranravindrant@gmail.com>
2021-07-16 01:06:41 +0530

    [kiran]Added:Main function of VendingMachineProgram

---
+++ VendingMachineProgram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MoneyVendingMachines
+{
+    class VendingMachineProgram
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the Amount");
+            Console.WriteLine("Available Denominations : 1, 2, 5, 10, 50, 100, 500 ,1000 ");
+            int amount=Convert.ToInt32(Console.ReadLine());
+            FindMinNotes.countCurrency(amount);
+
+
+        }
+    }
+}
