c3fdc58e957f7737e9fce31f34d4bf413790a8b5
kiranravindrant <kiranravindrant@gmail.com>
2021-07-16 14:38:47 +0530

    [kiran]Added:Class with SqRoot Method

---
+++ SquareRoot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SquareRootEpsilonMethod
+{
+    class SquareRoot
+    {
+        public static double SqRoot(double c,double t, double epsilon)
+        {
+            
+            while (Math.Abs(t - c / t) > epsilon * t)
+            {
+                t = ((c / t) + t) / 2;
+
+            }
+
+
+            return t;
+        }
+
+    }
+}
