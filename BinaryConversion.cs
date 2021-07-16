f324161b40e7edb4aece017e4e032bbaed057d13
kiranravindrant <kiranravindrant@gmail.com>
2021-07-16 17:10:11 +0530

    [kiran]Added:Decimal to Binary conversion program

---
+++ BinaryConversion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToBinary
+{
+    class BinaryConversion
+    {
+        public static string ToBinary(int n)
+        { string binvalue=" ";
+            int Length;
+
+            while (n >= 1)
+            {
+              
+                int remainder = n % 2;
+                n = n / 2;
+
+                binvalue += Convert.ToString(remainder);            //storing the remainders as string
+
+               
+
+            }
+                #region                                                        
+            string Revbinvalue = " ";                            //to reverse the string as the binary equivalent is opposite
+
+            Length = binvalue.Length - 1;
+
+            while (Length >= 0)                //loops the given string length
+            {
+
+                Revbinvalue = Revbinvalue + binvalue[Length];  //performimg a reverse string according to length of given string
+                Length--;
+
+            }
+            #endregion                                                      
+
+            return Revbinvalue;         //returning the reversed string
+
+          
+        } 
+
+
+    }
+
+
+
+}
