d1f4a3d97d3b43f9ad4a81188db3e6df7916b9fe
kiranravindrant <kiranravindrant@gmail.com>
2021-07-16 14:37:08 +0530

    [kiran]Added:Main method of SquareRoot Epsilon program

---
+++ MainProgram_sqroot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SquareRootEpsilonMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number: ");
+            double c = double.Parse(Console.ReadLine());
+            double t = c;
+        
+            double epsilon = 1e-15;
+
+            Console.WriteLine("Square root of "+c+" is "+SquareRoot.SqRoot(c, t, epsilon));    //displaying the return value of
+                                                                                            //the method Sqroot
+
+
+
+        }
+
+    }
+}
