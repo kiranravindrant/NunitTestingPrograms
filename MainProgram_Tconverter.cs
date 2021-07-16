b07e124ab910afe63e20e3d197edeae898c8727a
kiranravindrant <kiranravindrant@gmail.com>
2021-07-16 13:13:06 +0530

    [kiran]Added:Main method of Temperature Converter

---
+++ MainProgram_Tconverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TemperatureConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int option;
+            Console.WriteLine("-----Choose the Conversion------");
+            Console.WriteLine("1:Celsius to Fahrenheit: ");
+            Console.WriteLine("2:Fahrenheit to Celsius: ");
+            option=Convert.ToInt32(Console.ReadLine());
+
+      #region
+            //for displaying unit correctly
+            char u;
+            if (option == 2)
+            {
+                u ='C'; 
+            }
+            else
+            {
+                 u= 'F';
+            }                          
+      #endregion   
+
+            Console.WriteLine("The value in "+ u +"  is "+ Tconverter.ConvertTo(option));
+            Console.ReadLine();
+            
+
+         
+        }
+
+
+    }
+}
