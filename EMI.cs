45cf97b80399da039a1fd021f4aeff3626d9fd5d
kiranravindrant <kiranravindrant@gmail.com>
2021-07-16 14:01:57 +0530

    [kiran]Added:class added with Method of Monthlypay

---
+++ EMI.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonthlyPayment
+{
+    class EMI
+    {
+        
+        public static double MonthlyPay(int y,double p,double r)
+        {
+            int n;
+
+            n = 12 * y;
+            r /= (12 * 100);
+
+            double payment = (p * r) / (1 - Math.Pow((1 + r), (-n)));
+
+
+            return Math.Round(payment);
+
+        }
+
+
+    }
+}
