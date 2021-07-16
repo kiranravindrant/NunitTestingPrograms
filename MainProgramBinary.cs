eeaef665c1d52f9297141c5b46f5d9b521fdf7ac
kiranravindrant <kiranravindrant@gmail.com>
2021-07-16 17:14:50 +0530

    [kiran]Added:Main method of binary conversion Program

---
+++ MainProgramBinary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int n;
+            Console.WriteLine("Enter the Number to convert to Binary"  );
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The binary Equivalent is " + BinaryConversion.ToBinary(n));
+            
+            
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
