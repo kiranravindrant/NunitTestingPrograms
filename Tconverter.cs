cca7c49be5ec75570ebf9de581b56f3e121afe3d
kiranravindrant <kiranravindrant@gmail.com>
2021-07-16 13:22:34 +0530

    [kiran]Added:Class Tconverter of temperature conversion Program

---
+++ Tconverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemperatureConverter
+{
+    class Tconverter
+    {
+        public static double ConvertTo(int option)
+        {
+            double value;
+            double result = 0;
+
+            switch (option)
+            { 
+                case 1: Console.WriteLine("Enter value in Celsius");
+                    value=Convert.ToDouble(Console.ReadLine());
+                    result = value * (9 / 5) + 32;
+                    break;
+
+                case 2: Console.WriteLine("Enter value in Fahrenhiet ");
+                    value = Convert.ToDouble(Console.ReadLine());
+                    result = (value - 32) * (5 / 9);
+                    break;
+
+
+            }
+            return result;
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
+
+
+        
